a5dc96bd8bf5d15e453308958ac3d4bd6b0206bf
Charanreddy <charanreddysama990@gmail.com>
2026-01-31 00:25:51 +0530

    remove all files

--- WEEK-2/Dates/date3.js
+++
@@ -1,7 +0,0 @@
-// let dob = "2000-05-15";
-//Calculate exact age in years
-let dob="2000-05-15";
-let datedob=new Date(dob);
-let date=new Date();
-let age=date.getFullYear()-datedob.getFullYear();
-console.log("Age is: "+age);
